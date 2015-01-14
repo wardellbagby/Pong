d005a11a6813eb39cb8d356781b6544e4225f58d
djohnson <mr.demarcus.johnson@gmail.com>
2015-01-14 17:11:30 -0600

    project - organization -Getting the project setup together and organized

---
+++ Pong/Pong/Back End/GameInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pong.Back_End
+{
+    static class GameInfo
+    {
+        private
+            //Board info
+            Object[] paddles;
+            Object ball;
+            int[] scores;
+
+            //Menu Info
+            int playerAmount;
+            double ballSpeed;
+
+            String[] playerNames;
+
+        //Game Info
+
+
+
+
+            
+
+        public
+            int x;
+
+
+    }
+}
