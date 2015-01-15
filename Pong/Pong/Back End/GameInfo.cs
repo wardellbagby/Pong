5aa870d36ff7687cf40af7e85de04d215b07bfad
DJ <mr.demarcus.johnson@gmail.com>
2015-01-14 21:03:32 -0600

    Fixed up GameInfo class, should be easy to call all the variables now. I didnt try anything crazy, maybe i'll look into it later.

--- Pong/Pong/Back End/GameInfo.cs
+++ Pong/Pong/Back End/GameInfo.cs
@@ -5,29 +5,85 @@ using System.Text;
 
 namespace Pong.Back_End
 {
-    static class GameInfo
+    public class GameInfo
     {
-        private
-            //Board info
-            Object[] paddles;
-            Object ball;
-            int[] scores;
-
-            //Menu Info
-            int playerAmount;
-            double ballSpeed;
-
-            String[] playerNames;
-
-        //Game Info
 
 
+        //Board info
+        public static Object[] paddles;
+        public static Object ball;
+        public static int[] scores;
 
+        //Menu Info
+        public static int playerAmount;
+        public static double ballSpeed;
 
-            
+        public static String[] playerNames;
 
-        public
-            int x;
+        //Methods
+
+        public static Object[] getPaddles()
+        {
+            return paddles;
+        }
+
+        public static void setPaddles(Object[] newPaddles)
+        {
+            paddles = newPaddles;
+        }
+
+        public static Object getBall()
+        {
+            return ball;
+        }
+
+        public static void setBall(Object newBall)
+        {
+            ball = newBall;
+        }
+
+        public static int[] getScores()
+        {
+            return scores;
+        }
+
+        public static void setScores(int[] newScores)
+        {
+            scores = newScores;
+        }
+
+        public static int getPlayerAmount()
+        {
+            return playerAmount;
+        }
+
+        public static void setPlayerAmount(int newPlayerAmount)
+        {
+            if (playerAmount >= 0)
+            {
+                playerAmount = newPlayerAmount;
+            }
+        }
+
+        public static double getBallSpeed()
+        {
+            return ballSpeed;
+        }
+
+        public static void setBallSpeed(double newBallSpeed)
+        {
+            ballSpeed = newBallSpeed;
+        }
+
+        public static String[] getPlayerNames()
+        {
+            return playerNames;
+        }
+
+        public static void setPlayerNames(String[] newPlayerNames)
+        {
+            playerNames = newPlayerNames;
+        } 
 
 
     }
