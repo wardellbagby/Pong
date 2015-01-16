e581cc668839e368d77de7a15fb7e42b3660b56d
msparkman <mhsparkman@gmail.com>
2015-01-15 20:57:59 -0600

    Set up the basic classes for the ball, paddle, and board. Updated the game info class to include these new classes.

---
+++ Pong/Pong/Back End/Board.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pong.Back_End
+{
+    class Board
+    {
+        // Board stats
+        private static int width;
+        private static int height;
+
+        // Methods
+        public int getWidth()
+        {
+            return width;
+        }
+
+        public static void setWidth(int newWidth)
+        {
+            width = newWidth;
+        }
+
+        public int getHeight()
+        {
+            return height;
+        }
+
+        public static void setHeight(int newHeight)
+        {
+            height = newHeight;
+        }
+    }
+}
