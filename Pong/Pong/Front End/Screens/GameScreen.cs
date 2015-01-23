b166a5052674711c2fa1c23e0e591dab1eafd333
msparkman <mhsparkman@gmail.com>
2015-01-22 21:31:25 -0600

    Allowed the user to control the left paddle. Also added exit on the Esc key press.

--- Pong/Pong/Front End/Screens/GameScreen.cs
+++ Pong/Pong/Front End/Screens/GameScreen.cs
@@ -13,45 +13,57 @@ namespace Pong.Front_End.Screens
 {
     class GameScreen : Screen
     {
-        private Vector2 position;
+        private Vector2 playerPosition;
+        private Vector2 enemyPosition;
+        private int paddleWidth;
 
         public override void LoadAssets()
         {
             BackgroundColor = Color.Black;
             // TODO this paddle info is just for testing
-            Paddle paddle = new Paddle(10, 40);
+            paddleWidth = 10;
+            Paddle paddle = new Paddle(paddleWidth, 40);
             Info.setPaddles(new Paddle[] { paddle, paddle });
         }
 
         public override void Update(GameTime gameTime)
         {
-            Vector2 targetPosition = new Vector2(
-                    ScreenManager.GraphicsDeviceManager.GraphicsDevice.Viewport.Width / 2 - (ScreenManager.Fonts["Default"].MeasureString("Insert Gameplay Here").X / 2),
-                    ScreenManager.GraphicsDeviceManager.GraphicsDevice.Viewport.Height / 2 - (ScreenManager.Fonts["Default"].MeasureString("Insert Gameplay Here").Y / 2));
+            //Exit
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Environment.Exit(0);
+            }
 
-            position = Vector2.Lerp(position, targetPosition, 0.01f);
+            // This vector is where we want the left paddle to start
+            Vector2 playerStartPosition = new Vector2(0, Info.gameHeight / 2);
+
+            // This position is the one we want the player to control
+            playerPosition = Vector2.Lerp(playerPosition, playerStartPosition, 0.00f);
             if (Keyboard.GetState().IsKeyDown(Keys.Down))
             {
-                position.Y += 10;
+                playerPosition.Y += 10;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Up))
             {
-                position.Y -= 10;
+                playerPosition.Y -= 10;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Right))
             {
-                position.X += 10;
+                playerPosition.X += 10;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Left))
             {
-                position.X -= 10;
+                playerPosition.X -= 10;
             }
+
+            // TODO need to create a target position for the opponent's paddle to randomly move
+            Vector2 enemyStartPosition = new Vector2(Info.gameWidth - paddleWidth, Info.gameHeight / 2);
+            enemyPosition = Vector2.Lerp(enemyPosition, enemyStartPosition, 0.01f);
         }
 
         public override void Draw(GameTime gameTime)
         {
             ScreenManager.Sprites.Begin();
-            ScreenManager.Sprites.DrawString(ScreenManager.Fonts["Default"], "Put in some game stuff here...", position, Color.White);
 
             // Get the paddles from the game info
             Paddle[] paddles = Info.getPaddles();
@@ -64,7 +76,6 @@ namespace Pong.Front_End.Screens
             {
                 numberOfPaddles++;
 
-                int paddleWidth = paddle.getWidth();
                 if (paddleWidth <= 0)
                 {
                     continue;
@@ -76,7 +87,6 @@ namespace Pong.Front_End.Screens
                     continue;
                 }
 
-                // TODO need to figure out how to tie a name to this Texture2D so that we can reference it in the Update function.
                 Texture2D rectangleTexture = new Texture2D(graphicsDevice, paddleWidth, paddleHeight);
                 
                 // The following "data" stuff confuses me since the color doesn't actually show up, but if we don't do this then the rectangle won't show up.
@@ -90,12 +100,14 @@ namespace Pong.Front_End.Screens
                 // Need to place the paddles on opposite sides of the game board
                 if (numberOfPaddles % 2 == 1)
                 {
-                    ScreenManager.Sprites.Draw(rectangleTexture, new Rectangle(0, Info.gameHeight / 2, paddleWidth, paddleHeight), Color.Crimson);
+                    ScreenManager.Sprites.Draw(rectangleTexture, playerPosition, new Rectangle(0, Info.gameHeight / 2, paddleWidth, paddleHeight), Color.Crimson);
                 }
                 else
                 {
                     ScreenManager.Sprites.Draw(rectangleTexture, new Rectangle(Info.gameWidth - paddleWidth, Info.gameHeight / 2, paddleWidth, paddleHeight), Color.Gray);
                 }
+
+                // TODO need to draw the ball. It'll be easiest to just save a ball picture and draw it as a sprite.
             }
 
             ScreenManager.Sprites.End();
